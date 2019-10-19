65c23fd555319ef32802d0e7d1978835b45755cb
gotzmann <sgotsulyak@gmail.com>
2019-10-19 18:55:47 +0300

    MySQL: Create db and user

---
+++ mysql.sql
@@ -0,0 +1,4 @@
+-- CREATE DATABASE db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass'; GRANT ALL PRIVILEGES ON db.* TO 'user'@'localhost';
+
